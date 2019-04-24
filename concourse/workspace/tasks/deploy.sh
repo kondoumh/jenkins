c1c2939ae771e1e5ec09aa3c182c495bd4497d73
kondoumh <kondoumh@gmail.com>
2019-04-24 08:33:46 +0000

    add curl command

--- concourse/workspace/tasks/deploy.sh
+++ concourse/workspace/tasks/deploy.sh
@@ -6,4 +6,6 @@ cd sb-sample-service
 docker-compose up -d
 sleep 30
 docker ps
+curl -X POST "http://localhost:18888/api/user/" -H "accept: */*" -H "Content-Type: application/json" -d "{ \"id\": 1, \"name\": \"Mike\"}"
+curl -X GET "http://localhost:18888/api/usr/1" -H "accept: */*"
 docker-compose down
